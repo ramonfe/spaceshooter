4eb9b8f89851cb682303d4564e57733a73e92b6b
Ramon Felix <ramon.felix@zeiss.com>
2021-07-22 18:33:16 -0700

    adding gitignore good

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    [SerializeField]
+    float _speedVariable = 3.0f;
+    [SerializeField]
+    private int _powerupID;
+    //0=tripleshot
+    //1=speed
+    //2=shield
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speedVariable * Time.deltaTime);
+
+        if (transform.position.y <= -4.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            Player player = other.transform.GetComponent<Player>();
+            if (player != null)
+            {
+                switch (_powerupID)
+                {
+                    case 0:
+                        player.TripleShotActivate();
+                        break;
+                    case 1:
+                        player.SpeedUpActivated();
+                        break;
+                    case 2:
+                        player.ShieldActivated();
+                        break;
+                    default:
+                        Debug.Log("Shield collected");
+                        break;
+                }  
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Numerics;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _enemyPrefab;
+    [SerializeField]
+    GameObject _enemyContainer;
+    [SerializeField]
+    GameObject[] powerUps;
+    bool _stopSpawing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+        StartCoroutine(SpawnTripleShotRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator SpawnEnemyRoutine()
+    {
+        while (_stopSpawing == false)
+        {
+            Vector3 PrefabPosition = new Vector3(Random.Range(-8f, 8f), 7, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, PrefabPosition, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
+            yield return new WaitForSeconds(5.0f);
+        }
+
+    }
+    IEnumerator SpawnTripleShotRoutine()
+    {
+        while (_stopSpawing == false)
+        {
+            Vector3 PrefabPosition = new Vector3(Random.Range(-8, 8), 7, 0);
+            int powerUpRandom = Random.Range(0, 3);
+            Instantiate(powerUps[powerUpRandom], PrefabPosition, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(3, 8));
+        }
+    }
+    public void OnPlayerDeath()
+    {
+        _stopSpawing = true;
+    }
+}
